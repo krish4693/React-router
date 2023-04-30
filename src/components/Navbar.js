80213d379a630453deff339c80127b23ce684869
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-05-01 00:09:09 +0530

    navigating programmatically

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavLink  } from 'react-router-dom'
+
+export const Navbar = () => {
+    const navLinkStyles=({isActive})=>{
+        return{
+            fontWeight:isActive?'bold':'normal',
+            textDecoration:isActive?'none':'underline',
+        }
+
+    }
+
+  return (
+    <nav>
+        <NavLink to='/' style={navLinkStyles}>Home</NavLink>
+        <NavLink to='/about' style={navLinkStyles}>About</NavLink>
+        
+    </nav>
+  )
+}
---
+++ src/components/OrderSummary.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export const OrderSummary = () => {
+    const navigate=useNavigate()
+  return (
+    <>
+        <div>Order Placed!</div>
+        <button onClick={()=>navigate('/')}>Go Back</button>
+    </>
+
+  )
+}
+
