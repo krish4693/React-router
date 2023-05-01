e3cf714b4b7f060883cfca49e090e8202e0d177f
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-05-01 10:00:29 +0530

    added nested routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, Router} from 'react-router-dom'
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Navbar } from './components/Navbar';
 import { OrderSummary } from './components/OrderSummary';
 import { NoMatch } from './components/NoMatch.';
 import { Product } from './components/Products';
+import { FeaturedProducts } from './components/FeaturedProducts';
+import { NewProducts } from './components/NewProducts';
 
 function App() {
   return(
@@ -15,7 +17,10 @@ function App() {
       <Route path='/about' element={<About/>}></Route>
       <Route path='/order-summary' element={<OrderSummary/>}></Route>
       <Route path='*' element={<NoMatch/>}></Route>
-      <Route path='/products' element={<Product/>}/>
+      <Route path='/products' element={<Product/>}>
+        <Route path='featured' element={<FeaturedProducts/>}/>
+        <Route path='new' element={<NewProducts/>}/>
+      </Route>
     </Routes>
     </>
   );
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 export const Product= () => {
   return (
+    <>
     <div> 
-        <input placeholder='search items'/>
+        <input placeholder='search items' type='search '/>
     </div>
+    <nav>
+      <Link to='featured'>Featured</Link>
+      <Link to='new'>New</Link>
+    </nav>
+    <Outlet/>
+    </>
   )
 }
